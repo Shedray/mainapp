d62ddbcb5df9609679443051da055285bf061760
GitHub Action <action@github.com>
2022-07-06 02:45:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/shedray/mainapp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shedray/mainapp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shedray.mainapp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shedray.mainapp.MyBundle
 
 class MyApplicationService {
 
